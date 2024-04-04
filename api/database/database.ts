e0419d888d7ae90280a39e6803e10ab35c0d722c
EIJI01 <chanyut281144@gmail.com>
2024-04-04 23:21:45 +0700

    Add Login API

--- api/database/database.ts
+++ api/database/database.ts
@@ -1,7 +1,16 @@
-const firebase = require("firebase-admin");
-const serviceAccount = require("path");
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBLXZ53TI5-g2Cy7JWi_WowSdqQ7ZAvin4",
+  authDomain: "kkuremindyou.firebaseapp.com",
+  databaseURL: "https://kkuremindyou-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kkuremindyou",
+  storageBucket: "kkuremindyou.appspot.com",
+  messagingSenderId: "715076785074",
+  appId: "1:715076785074:web:7de6215548ff7f7e71caab",
+  measurementId: "G-97M74MVYG9",
+};
 
-const database = firebase.firestore();
+const app = initializeApp(firebaseConfig);
 
-export default database;
+export default app;
--- api/routes/authentication/authentication.ts
+++ api/routes/authentication/authentication.ts
@@ -3,6 +3,6 @@ import express from "express";
 const authenticationController = require("../../controllers/authentication/authentication");
 const router = express.Router();
 
-router.get("/login", authenticationController.login);
+router.post("/login", authenticationController.login);
 
 module.exports = router;
