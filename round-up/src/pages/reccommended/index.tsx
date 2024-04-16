f0c3b36dfe8a548bc0c73cd1861de8c132dab74a
EIJI01 <chanyut281144@gmail.com>
2024-04-16 22:29:51 +0700

    Add filter value and change default page

--- round-up/src/pages/reccommended/index.tsx
+++ round-up/src/pages/reccommended/index.tsx
@@ -52,9 +52,7 @@ export default function ThankYou() {
       </Box>
 
       <Box sx={{ width: "100%", height: "fit-content" }}>
-        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>
-          Remind You คืออะไร?
-        </Typography>
+        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>Remind You คืออะไร?</Typography>
         <Typography
           sx={{
             fontSize: "18px",
@@ -62,15 +60,12 @@ export default function ThankYou() {
             marginTop: "5px",
           }}
         >
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เป็นเว็ปแอพพลิเคชันที่ถูกสร้างมาเพื่อช่วยกระจายข่าวสาร
-          และการจัดกิจกรรม โดยผู้ใช้งานจะสามารถฟิลเตอร์หาเฉพาะ
-          ประเภทกิจกรรมที่ตนสนใจได้
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เป็นเว็ปแอพพลิเคชันที่ถูกสร้างมาเพื่อช่วยกระจายข่าวสาร และการจัดกิจกรรม
+          โดยผู้ใช้งานจะสามารถฟิลเตอร์หาเฉพาะ ประเภทกิจกรรมที่ตนสนใจได้
         </Typography>
       </Box>
       <Box sx={{ width: "100%", height: "fit-content" }}>
-        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>
-          Remind You มีประโยชน์อย่างไร?
-        </Typography>
+        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>Remind You มีประโยชน์อย่างไร?</Typography>
         <Typography
           sx={{
             fontSize: "18px",
@@ -78,17 +73,13 @@ export default function ThankYou() {
             marginTop: "5px",
           }}
         >
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ตัวเว็ปถูกสร้างมาเพื่อแก้ปัญหาข่าวสารของอีเวนท์ที่มีการ
-          ประกาศหลายที่มากๆ ซึ่งในหลายๆครั้ง ข่าวสารไปไม่ถึงผู้ที่ต้อง
-          การอย่างทันเวลา รวมไปถึงการนับจำนวนผู้เข้าร่วมอีเวนท์ ที่เร็วๆนี้
-          ทางผู้จัดทำยังพบเห็นวิธีการนับคนเข้างานด้วยสติ๊กเกอร์กันอยู่
-          เราจึงจัดทำเว็ปแอพพลิเคชันนี้ขึ้นมาเพื่อจัดการกับปัญหาเหล่าน
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ตัวเว็ปถูกสร้างมาเพื่อแก้ปัญหาข่าวสารของอีเวนท์ที่มีการ ประกาศหลายที่มากๆ ซึ่งในหลายๆครั้ง
+          ข่าวสารไปไม่ถึงผู้ที่ต้อง การอย่างทันเวลา รวมไปถึงการนับจำนวนผู้เข้าร่วมอีเวนท์ ที่เร็วๆนี้
+          ทางผู้จัดทำยังพบเห็นวิธีการนับคนเข้างานด้วยสติ๊กเกอร์กันอยู่ เราจึงจัดทำเว็ปแอพพลิเคชันนี้ขึ้นมาเพื่อจัดการกับปัญหาเหล่าน
         </Typography>
       </Box>
       <Box sx={{ width: "100%", height: "fit-content" }}>
-        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>
-          Remind You ใช้งานอย่างไร?
-        </Typography>
+        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>Remind You ใช้งานอย่างไร?</Typography>
         <Typography
           sx={{
             fontSize: "18px",
@@ -96,18 +87,13 @@ export default function ThankYou() {
             marginTop: "5px",
           }}
         >
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;หลังจากสร้างบัญชีผู้ใช้แล้วเข้าสู่ระบบ
-          ในหน้าแรกจะมีอีเวนท์ ทั้งหมดของทั้งมหาวิทยาลัยที่กำลังจะจัด
-          โดยสามารถใส่ฟิลเตอร์ ดูเฉพาะกิจกรรมในบางคณะ หรือบางประเภทที่สนใจได้
-          ในกิจกรรมที่มีการจำกัดจำนวนคน แค่เพียงผู้ใช้งานกดจองที่ เจ้าหน้าที่ของ
-          กิจกรรมก็จะติดต่อกลับไปยืนยันกับเราได้เลย
-          โดยไม่ต้องกรอกข้อมูลอะไรเพิ่มเติม
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;หลังจากสร้างบัญชีผู้ใช้แล้วเข้าสู่ระบบ ในหน้าแรกจะมีอีเวนท์
+          ทั้งหมดของทั้งมหาวิทยาลัยที่กำลังจะจัด โดยสามารถใส่ฟิลเตอร์ ดูเฉพาะกิจกรรมในบางคณะ หรือบางประเภทที่สนใจได้ ในกิจกรรมที่มีการจำกัดจำนวนคน
+          แค่เพียงผู้ใช้งานกดจองที่ เจ้าหน้าที่ของ กิจกรรมก็จะติดต่อกลับไปยืนยันกับเราได้เลย โดยไม่ต้องกรอกข้อมูลอะไรเพิ่มเติม
         </Typography>
       </Box>
       <Box sx={{ width: "100%", height: "fit-content" }}>
-        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>
-          นอกจากเรื่องข่าวสารแล้ว มีประโยชน์อะไรอีกไหม?
-        </Typography>
+        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>นอกจากเรื่องข่าวสารแล้ว มีประโยชน์อะไรอีกไหม?</Typography>
         <Typography
           sx={{
             fontSize: "18px",
@@ -115,18 +101,14 @@ export default function ThankYou() {
             marginTop: "5px",
           }}
         >
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;นอกจากฝั่งของผู้เข้าร่วมกิจกรรมแล้ว
-          จุดประสงค์หลัก อีกอย่างของเว็ปแอพลิเคชันนี้คือช่วยเหลือผู้จัดกิจกรรม
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เมื่อผู้จัดกิจกรรมทำการเพิ่มกิจกรรม
-          ทางผู้จัดจะมองเห็น รายละเอียด ช่องทางการติดต่อ
-          ของผู้ที่ต้องการจะเข้าร่วมทุกคน และนอกจากนั้น เมื่อผู้คนมาเข้างาน
-          ข้อมูลของผู้เข้าร่วมงานจริง ก็จะถูกแสดงให้ผู้จัดงานทราบด้วยเช่นกัน
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;นอกจากฝั่งของผู้เข้าร่วมกิจกรรมแล้ว จุดประสงค์หลัก
+          อีกอย่างของเว็ปแอพลิเคชันนี้คือช่วยเหลือผู้จัดกิจกรรม &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เมื่อผู้จัดกิจกรรมทำการเพิ่มกิจกรรม
+          ทางผู้จัดจะมองเห็น รายละเอียด ช่องทางการติดต่อ ของผู้ที่ต้องการจะเข้าร่วมทุกคน และนอกจากนั้น เมื่อผู้คนมาเข้างาน ข้อมูลของผู้เข้าร่วมงานจริง
+          ก็จะถูกแสดงให้ผู้จัดงานทราบด้วยเช่นกัน
         </Typography>
       </Box>
       <Box sx={{ width: "100%", height: "fit-content" }}>
-        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>
-          จะเริ่มต้นใช้งานได้อย่างไร?
-        </Typography>
+        <Typography sx={{ fontSize: "32px", fontWeight: "700" }}>จะเริ่มต้นใช้งานได้อย่างไร?</Typography>
         <Typography
           sx={{
             fontSize: "18px",
@@ -134,8 +116,7 @@ export default function ThankYou() {
             marginTop: "5px",
           }}
         >
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;สามารถคลิกที่ปุ่มด้านล่าง
-          เพื่อเริ่มทดลองใช้งานได้เลย
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;สามารถคลิกที่ปุ่มด้านล่าง เพื่อเริ่มทดลองใช้งานได้เลย
         </Typography>
       </Box>
       <Button
@@ -151,7 +132,7 @@ export default function ThankYou() {
           paddingY: "12px",
         }}
         onClick={() => {
-          router.push("register");
+          router.push("/");
         }}
       >
         เริ่มต้นใช้งาน
